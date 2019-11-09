cfc6efcf2783b54071b34a8ec1f7a04808944226
DanielMagen <daniel.magen@gmail.com>
2019-11-09 15:19:43 +0200

    started working on split and merge of arnodes

--- second_try/Node.py
+++ second_try/Node.py
@@ -57,6 +57,43 @@ class Node:
         else:
             self.outgoing_edges_manager = NodeEdges(number_of_tables_in_next_layer)
 
+        # if this value is true then the node can not do any action
+        # this value would be deprecated in the cpp implementation, as a call to the destructor would
+        # make it unnecessary
+        self.finished_lifetime = False
+
+    def destructor(self):
+        """
+        removes the node from all of its neighbors,
+        and sets the node finished_lifetime to True
+
+        it does not remove the node from its residing table, its up to the table to do so.
+        """
+
+        def remove_from_node_by_direction_and_data(direction_of_connection, connection_data):
+            table_number, index_in_table, weight, neighbor = connection_data
+            neighbor_location_data = [table_number, index_in_table]
+            neighbor.remove_neighbor_from_neighbors_list(direction_of_connection,
+                                                         neighbor_location_data,
+                                                         remove_this_node_from_given_node_neighbors_list=False)
+
+        # if an node is connected to us by an edge that is incoming to us it means that for him we are
+        # an outgoing connection
+        for data in self.get_iterator_for_incoming_edges_data():
+            remove_from_node_by_direction_and_data(Node.OUTGOING_EDGE_DIRECTION, data)
+
+        for data in self.get_iterator_for_outgoing_edges_data():
+            remove_from_node_by_direction_and_data(Node.INCOMING_EDGE_DIRECTION, data)
+
+        self.finished_lifetime = True
+
+    def check_if_killed(self):
+        return self.finished_lifetime
+
+    def _check_if_killed_and_raise_error_if_is(self):
+        if self.check_if_killed():
+            raise Exception("this arnode is dead and can not support any function")
+
     def get_number_of_tables_in_previous_layer(self):
         return self.incoming_edges_manager.get_number_of_tables_in_layer_connected_to()
 
@@ -93,6 +130,8 @@ class Node:
         """
         :return: [table_number, index_in_table]
         """
+        self._check_if_killed_and_raise_error_if_is()
+
         # using assumption (2), since the node can not be moved to another layer we do not include the layer number
         # in the id. hence the node id is unique only in the preview of the layer its in
         return [self.table_number, self.index_in_table]
@@ -106,9 +145,13 @@ class Node:
             raise Exception("the node location cannot be changed")
 
     def get_iterator_for_incoming_edges_data(self):
+        self._check_if_killed_and_raise_error_if_is()
+
         return self.incoming_edges_manager.get_iterator_over_connections()
 
     def get_iterator_for_outgoing_edges_data(self):
+        self._check_if_killed_and_raise_error_if_is()
+
         return self.outgoing_edges_manager.get_iterator_over_connections()
 
     def add_neighbor(self, direction_of_connection, weight, node, add_this_node_to_given_node_neighbors=False):
@@ -121,6 +164,8 @@ class Node:
         (from the right direction of course)
         :return:
         """
+        self._check_if_killed_and_raise_error_if_is()
+
         if direction_of_connection == Node.INCOMING_EDGE_DIRECTION:
             edges_manager_to_work_with = self.incoming_edges_manager
         elif direction_of_connection == Node.OUTGOING_EDGE_DIRECTION:
@@ -134,15 +179,17 @@ class Node:
         if add_this_node_to_given_node_neighbors:
             node.add_neighbor(-direction_of_connection, weight, self, add_this_node_to_given_node_neighbors=False)
 
-    def delete_neighbor(self, direction_of_connection, neighbor_location_data,
-                        delete_this_node_to_given_node_neighbors=False):
+    def remove_neighbor_from_neighbors_list(self, direction_of_connection, neighbor_location_data,
+                                            remove_this_node_from_given_node_neighbors_list=False):
         """
         :param direction_of_connection:
         :param neighbor_location_data:
-        :param delete_this_node_to_given_node_neighbors: if true would delete this node from the given node neighbors
-        (from the right direction of course)
+        :param remove_this_node_from_given_node_neighbors_list: if true would delete this node from the given
+        node neighbors (from the right direction of course)
         :return:
         """
+        self._check_if_killed_and_raise_error_if_is()
+
         if direction_of_connection == Node.INCOMING_EDGE_DIRECTION:
             edges_manager_to_work_with = self.incoming_edges_manager
         elif direction_of_connection == Node.OUTGOING_EDGE_DIRECTION:
@@ -153,9 +200,9 @@ class Node:
         table_number, index_in_table = neighbor_location_data
         _, node_connected_to = edges_manager_to_work_with.delete_connection(table_number, index_in_table)
 
-        if delete_this_node_to_given_node_neighbors:
-            node_connected_to.delete_neighbor(-direction_of_connection, self.get_location(),
-                                              delete_this_node_to_given_node_neighbors=False)
+        if remove_this_node_from_given_node_neighbors_list:
+            node_connected_to.remove_neighbor_from_neighbors_list(-direction_of_connection, self.get_location(),
+                                                                  remove_this_node_from_given_node_neighbors_list=False)
 
     def get_notified_that_neighbor_location_changed(self, direction_of_connection, previous_location, new_location):
         """
@@ -169,6 +216,8 @@ class Node:
         :param new_location: the new location of the node that was changed
         :return:
         """
+        self._check_if_killed_and_raise_error_if_is()
+
         if direction_of_connection == Node.INCOMING_EDGE_DIRECTION:
             edges_manager_to_work_with = self.incoming_edges_manager
         elif direction_of_connection == Node.OUTGOING_EDGE_DIRECTION:
@@ -190,6 +239,8 @@ class Node:
         goes over all neighbors of the node and notifies them that this node has been moved to another location
         within the layer (using assumption (2))
         """
+        self._check_if_killed_and_raise_error_if_is()
+
         new_location = self.get_location()
 
         # first go over all incoming edges
@@ -211,6 +262,7 @@ class Node:
 
     def change_location(self, table_number, index_in_table):
         self._check_if_location_can_be_changed_and_raise_error_if_not()
+        self._check_if_killed_and_raise_error_if_is()
 
         previous_location = self.get_location()
         self.table_number = table_number
@@ -241,15 +293,11 @@ class ARNode(Node):
     def __init__(self,
                  starting_node,
                  table_number,
-                 index_in_table,
-                 function_to_calculate_merger_of_incoming_edges,
-                 function_to_calculate_merger_of_outgoing_edges):
+                 index_in_table):
         """
         :param starting_node:
         :param table_number:
         :param index_in_table:
-        :param function_to_calculate_merger_of_incoming_edges:
-        :param function_to_calculate_merger_of_outgoing_edges:
         """
         number_of_tables_in_previous_layer = starting_node.get_number_of_tables_in_previous_layer()
         number_of_tables_in_next_layer = starting_node.get_number_of_tables_in_next_layer()
@@ -267,11 +315,9 @@ class ARNode(Node):
         if not starting_node.check_if_location_can_be_changed():
             # assumption (2) is violated
             raise Exception("arnodes can only contain nodes which can not change their location")
-        self.inner_nodes = {starting_node}
-        starting_node.set_pointer_to_ar_node_nested_in(self.get_location())
 
-        self.function_to_calculate_merger_of_incoming_edges = function_to_calculate_merger_of_incoming_edges
-        self.function_to_calculate_merger_of_outgoing_edges = function_to_calculate_merger_of_outgoing_edges
+        self.inner_nodes = [starting_node]
+        starting_node.set_pointer_to_ar_node_nested_in(self.get_location())
 
         self.activation_status = ARNode.NOT_ACTIVATED_STATUS
 
@@ -291,7 +337,7 @@ class ARNode(Node):
     def check_if_location_can_be_changed(self):
         return False
 
-    def get_status(self):
+    def get_activation_status(self):
         return self.activation_status
 
     def forward_activate_arnode(self, add_this_node_to_given_node_neighbors=False):
@@ -305,6 +351,8 @@ class ARNode(Node):
         if true, it will set the add_this_node_to_given_node_neighbors in the add_neighbor function to true.
 
         """
+        self._check_if_killed_and_raise_error_if_is()
+
         if self.activation_status == ARNode.FULLY_ACTIVATED_STATUS:
             # from assumptions (6)
             raise Exception("can not demote in activation status")
@@ -312,8 +360,7 @@ class ARNode(Node):
         if self.activation_status == ARNode.ONLY_FORWARD_ACTIVATED_STATUS:
             return
 
-        # else self.activation_status == ARNode.NOT_ACTIVATED_STATUS from assumption (5)
-        self.activation_status = ARNode.ONLY_FORWARD_ACTIVATED_STATUS
+        # else, self.activation_status == ARNode.NOT_ACTIVATED_STATUS from assumption (5)
 
         # since before calling this function self.activation_status == ARNode.NOT_ACTIVATED_STATUS
         # from assumptions (4) we conclude that the arnode only contains 1 node - the starting_node it received
@@ -322,10 +369,7 @@ class ARNode(Node):
 
         direction_of_connection = ARNode.OUTGOING_EDGE_DIRECTION
 
-        starting_node = None
-        # this would only iterate once
-        for node in self.inner_nodes:
-            starting_node = node
+        starting_node = self.inner_nodes[0]
 
         # from assumption (5) we conclude that all the nodes that we are connected to by an outgoing edge can not
         # be fully activated yet as full activation requires this node to be forward activated
@@ -346,6 +390,9 @@ class ARNode(Node):
                               arnode_containing_node,
                               add_this_node_to_given_node_neighbors)
 
+        # finally, set the right activation status
+        self.activation_status = ARNode.ONLY_FORWARD_ACTIVATED_STATUS
+
     def fully_activate_arnode(self, add_this_node_to_given_node_neighbors=False):
         """
         this method fully activates the arnode.
@@ -356,6 +403,8 @@ class ARNode(Node):
         notify this neighbor to have him add us as his neighbor.
         if true, it will set the add_this_node_to_given_node_neighbors in the add_neighbor function to true.
         """
+        self._check_if_killed_and_raise_error_if_is()
+
         if self.activation_status == ARNode.FULLY_ACTIVATED_STATUS:
             return
 
@@ -369,8 +418,6 @@ class ARNode(Node):
 
         # now from assumption (5) we get self.activation_status = ARNode.ONLY_FORWARD_ACTIVATED_STATUS
 
-        self.activation_status = ARNode.FULLY_ACTIVATED_STATUS
-
         # all the outgoing edges were already added using the forward_activate_arnode function
         # simply add all the incoming edges
 
@@ -381,10 +428,7 @@ class ARNode(Node):
 
         direction_of_connection = ARNode.INCOMING_EDGE_DIRECTION
 
-        starting_node = None
-        # this would only iterate once
-        for node in self.inner_nodes:
-            starting_node = node
+        starting_node = self.inner_nodes[0]
 
         # from assumptions (5) we conclude that the incoming edges arnodes could not have been fully activated since it
         # would entail us to have been fully activated first, but we are only now entering the fully activated status.
@@ -402,7 +446,7 @@ class ARNode(Node):
 
             arnode_containing_node = node_connected_to.get_pointer_to_ar_node_nested_in()
 
-            if arnode_containing_node.get_status() != ARNode.ONLY_FORWARD_ACTIVATED_STATUS:
+            if arnode_containing_node.get_activation_status() != ARNode.ONLY_FORWARD_ACTIVATED_STATUS:
                 raise Exception("the arnode could not be fully activated before fully activating all the arnodes its"
                                 "connected to by a outgoing edge")
 
@@ -410,3 +454,36 @@ class ARNode(Node):
                               weight,
                               arnode_containing_node,
                               add_this_node_to_given_node_neighbors)
+
+            # finally, set the right activation status
+            self.activation_status = ARNode.FULLY_ACTIVATED_STATUS
+
+    def split(self, partition_of_arnode_inner_nodes):
+        pass
+
+    @staticmethod
+    def merge_two_arnodes(arnode1,
+                          arnode2,
+                          layer_number, table_number, index_in_table,
+                          function_to_calculate_merger_of_incoming_edges,
+                          function_to_calculate_merger_of_outgoing_edges):
+        """
+        this method merges the two arnodes given into a single arnode.
+        the merged arnode would contain all the inner nodes that both arnodes contained.
+
+        the 2 arnodes given are killed - i.e. they are removed from their neighbors and no action can be preformed on
+        them from here on out.
+
+
+        :param arnode1: a fully activated arnode
+        :param arnode2: a fully activated arnode
+
+        data required for the creation of the new arnode:
+        :param layer_number:
+        :param table_number:
+        :param index_in_table:
+        :param function_to_calculate_merger_of_incoming_edges:
+        :param function_to_calculate_merger_of_outgoing_edges:
+        :return: the new merged arnode
+        """
+        pass
