5f0ce7d3f87df72af52eb11a2379dd00891ab0d0
DanielMagen <daniel.magen@gmail.com>
2019-11-14 20:00:33 +0200

    updated table assumptions

--- second_try/Layer.py
+++
@@ -1,6 +0,0 @@
-"""
-in each layer we would have a vector of 5 table objects that would represent
-nodes from all possible kinds in the following order:
-(pos,inc), (pos,dec), (neg,inc), (neg,dec), (unprocessed)
-
-"""
