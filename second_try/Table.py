09b17898fe3b63652ce36747102d526df2f61b91
DanielMagen <daniel.magen@gmail.com>
2019-11-15 11:30:53 +0200

    moved some function from node to table to avoid circular dependency

--- second_try/Table.py
+++ second_try/Table.py
@@ -1,3 +1,6 @@
+from second_try.Node import Node, ARNode
+
+
 class TableAbstract:
     # the table_starting_index that should be given if there are no nodes in table
     INDEX_OF_START_IF_NO_NODES_IN_TABLE = -1
@@ -126,14 +129,38 @@ class TableAbstract:
     def remove_node_from_table_and_relocate_to_other_table(self, node_key, new_table_manager):
         raise NotImplemented("this is an abstract class")
 
-    def _add_node_to_table_helper(self, node):
+    def _add_node_to_table_without_checking(self, node):
         """
         this helper function would be implemented by the subclasses
-        it simply adds the node to the table and returns the key of the new node
+        it simply adds the node to the table and returns the key of the new node.
+        it does not check or change anything about the node
         :param node:
         :return the key of the node inserted
         """
         raise NotImplemented("this is an abstract class")
+
+    def create_new_node_and_add_to_table(self,
+                                         number_of_tables_in_previous_layer,
+                                         number_of_tables_in_next_layer,
+                                         number_of_tables_in_previous_layer_that_support_deletion,
+                                         number_of_tables_in_next_layer_that_support_deletion):
+        """
+        :param number_of_tables_in_previous_layer:
+        :param number_of_tables_in_next_layer:
+        :param number_of_tables_in_previous_layer_that_support_deletion:
+        :param number_of_tables_in_next_layer_that_support_deletion:
+        :return: the node created
+        """
+        new_node = Node(number_of_tables_in_previous_layer,
+                        number_of_tables_in_next_layer,
+                        number_of_tables_in_previous_layer_that_support_deletion,
+                        number_of_tables_in_next_layer_that_support_deletion,
+                        -1, -1)
+
+        node_key = self._add_node_to_table_without_checking(new_node)
+        new_node.set_new_table_without_checking(self.table_number, node_key)
+
+        return new_node
 
     def add_existing_node_to_table(self, previous_table_manager, node):
         """
@@ -151,7 +178,7 @@ class TableAbstract:
 
         previous_location = node.get_location()
 
-        new_node_key = self._add_node_to_table_helper(node)
+        new_node_key = self._add_node_to_table_without_checking(node)
 
         # change the inserted node location_data so that its table number and index would correspond to its new location
         node.set_new_table_without_checking(self.table_number, new_node_key)
@@ -188,7 +215,7 @@ class TableDoesntSupportsDeletion(TableAbstract):
     def get_number_of_nodes_in_table(self):
         return len(self.nodes)
 
-    def _add_node_to_table_helper(self, node):
+    def _add_node_to_table_without_checking(self, node):
         self.nodes.append(node)
         return len(self.nodes) - 1
 
@@ -213,7 +240,7 @@ class TableSupportsDeletion(TableAbstract):
     def get_number_of_nodes_in_table(self):
         return self.number_of_nodes
 
-    def _add_node_to_table_helper(self, node):
+    def _add_node_to_table_without_checking(self, node):
         new_key_for_node = self.number_of_nodes
         self.nodes[new_key_for_node] = node
         self.number_of_nodes += 1
@@ -244,3 +271,99 @@ class TableSupportsDeletion(TableAbstract):
         new_table_manager.add_existing_node_to_table(node_to_relocate)
 
         self._reset_key_of_node_currently_being_removed_from_table()
+
+
+class ARNodeTable(TableSupportsDeletion):
+    def create_new_node_and_add_to_table(self,
+                                         number_of_tables_in_previous_layer,
+                                         number_of_tables_in_next_layer,
+                                         number_of_tables_in_previous_layer_that_support_deletion,
+                                         number_of_tables_in_next_layer_that_support_deletion):
+        raise NotImplemented("this class can only contain arnodes")
+
+    def create_new_arnode_and_add_to_table(self, starting_nodes):
+        """
+
+        :param starting_nodes:
+        :return: the arnode created
+        """
+        new_node = ARNode(starting_nodes, -1, -1)
+
+        node_key = self._add_node_to_table_without_checking(new_node)
+        new_node.set_new_table_without_checking(self.table_number, node_key)
+
+        return new_node
+
+    def split_arnode(self, arnodes_to_split,
+                     partition_of_arnode_inner_nodes,
+                     function_to_calculate_merger_of_incoming_edges,
+                     function_to_calculate_merger_of_outgoing_edges
+                     ):
+        """
+        :param arnodes_to_split:
+        :param partition_of_arnode_inner_nodes: a list of lists which would be a valid partition of the
+        arnode inner nodes.
+        each sub list would contain a list of nodes references all of which are currently owned by
+        the given arnodes_to_split
+
+        those functions would be used to calculate the edges of each resulting arnode
+        :param function_to_calculate_merger_of_incoming_edges:
+        :param function_to_calculate_merger_of_outgoing_edges:
+
+        removes the arnode from the table its in and inserts the new arnodes created
+        """
+        # maybe add a function to check that the partition is valid
+
+        if arnodes_to_split.get_activation_status() != ARNode.FULLY_ACTIVATED_STATUS:
+            raise Exception("can not split a non fully activated arnode")
+
+        # before continuing, delete the arnode that needs to be split
+        # this would also reset the nodes owner arnode
+        arnodes_to_split.destructor()
+
+        for node_list in partition_of_arnode_inner_nodes:
+            # start node with bogus arguments and then add it to the table_manager which would give the node
+            # real arguments
+            new_arnode = self.create_new_arnode_and_add_to_table(node_list)
+            new_arnode.fully_activate_arnode(function_to_calculate_merger_of_incoming_edges,
+                                             function_to_calculate_merger_of_outgoing_edges,
+                                             add_this_node_to_given_node_neighbors=True)
+
+    def merge_two_arnodes(self,
+                          arnode1,
+                          arnode2,
+                          function_to_calculate_merger_of_incoming_edges,
+                          function_to_calculate_merger_of_outgoing_edges):
+        """
+        this method merges the two arnodes given into a single arnode.
+        the merged arnode would contain all the inner nodes that both arnodes contained.
+
+        the 2 arnodes given are killed - i.e. they are removed from their neighbors and no action can be preformed on
+        them from here on out.
+
+
+        :param arnode1: a fully activated arnode
+        :param arnode2: a fully activated arnode
+
+        those functions would be used to calculate the edges of each resulting arnode
+        :param function_to_calculate_merger_of_incoming_edges:
+        :param function_to_calculate_merger_of_outgoing_edges:
+        :return: the new merged arnode
+        """
+        for arnode in [arnode1, arnode2]:
+            if arnode.get_activation_status() != ARNode.FULLY_ACTIVATED_STATUS:
+                raise Exception("can not merge a non fully activated arnode")
+
+        # from assumption (2) both arnodes have the same table_manager
+        inner_nodes_for_new_arnode = arnode1.inner_nodes.union(arnode2.inner_nodes)
+
+        # before continuing, delete the arnodes that needs to be merged
+        # this would also reset the nodes owner arnode
+        arnode1.destructor()
+        arnode2.destructor()
+
+        new_arnode = self.create_new_arnode_and_add_to_table(inner_nodes_for_new_arnode)
+
+        new_arnode.fully_activate_arnode(function_to_calculate_merger_of_incoming_edges,
+                                         function_to_calculate_merger_of_outgoing_edges,
+                                         add_this_node_to_given_node_neighbors=True)
