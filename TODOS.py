4be9bf4c5cda8dee281d880c695275dea5fd2d63
DanielMagen <daniel.magen@gmail.com>
2019-12-06 15:49:00 +0200

    minor code maintenance

--- TODOS.py
+++ TODOS.py
@@ -1,11 +1,7 @@
 # TODO figure out how to link actions in the nodes, tables and layers objects to actions in the real network
-# TODO check if you can maintain table assumption 1
 
 # TODO add to the full explanation the various complexities of various important actions
 
 # TODO create a struct to hold connection data
 
 # TODO read and implement guy's method of converting the given network to have only one output
-
-# TODO the tables starting index takes a lot of resources to maintain. if we call it only rarely it might be preferable
-#  to simply remove it and set a method to compute it from scratch each time we need it. check if you really need it.
