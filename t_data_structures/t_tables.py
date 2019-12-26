aab4d85f8ab044a4b795b2209bda4f8fc7f4404a
DanielMagen <daniel.magen@gmail.com>
2019-12-26 17:03:37 +0200

    removed old code from t_tables that checked assumption (2)

--- t_data_structures/t_tables.py
+++ t_data_structures/t_tables.py
@@ -16,20 +16,17 @@ def create_tables_of_same_type(t, num_to_create):
 
 
 def get_TableSupportsDeletion(number_of_tables):
-    t1 = TableSupportsDeletion.TableSupportsDeletion(0, AbstractTable.NO_PREVIOUS_TABLE,
-                                                     AbstractTable.NO_NEXT_TABLE)
+    t1 = TableSupportsDeletion.TableSupportsDeletion(0)
     return create_tables_of_same_type(t1, number_of_tables - 1)
 
 
 def get_TableDoesntSupportsDeletion(number_of_tables):
-    t1 = TableDoesntSupportsDeletion.TableDoesntSupportsDeletion(0, AbstractTable.NO_PREVIOUS_TABLE,
-                                                                 AbstractTable.NO_NEXT_TABLE)
+    t1 = TableDoesntSupportsDeletion.TableDoesntSupportsDeletion(0)
     return create_tables_of_same_type(t1, number_of_tables - 1)
 
 
 def get_ARNodeTable(number_of_tables):
-    t1 = ARNodeTable.ARNodeTable(0, AbstractTable.NO_PREVIOUS_TABLE,
-                                 AbstractTable.NO_NEXT_TABLE)
+    t1 = ARNodeTable.ARNodeTable(0)
     return create_tables_of_same_type(t1, number_of_tables - 1)
 
 
@@ -38,112 +35,17 @@ def check_all_tables_have_zero_nodes(tables):
         assert t.get_number_of_nodes_in_table() == 0
 
 
-def check_starting_index_is_not_initialized(tables):
-    for t in tables:
-        t.decrease_starting_node_index()
-
-    for t in tables:
-        assert t.table_starting_index == AbstractTable.INDEX_OF_START_IF_NO_NODES_IN_TABLE
-
-    for t in tables:
-        t.increase_starting_node_index()
-
-    for t in tables:
-        assert t.table_starting_index == AbstractTable.INDEX_OF_START_IF_NO_NODES_IN_TABLE
-
-    for t in tables:
-        assert t.get_number_of_nodes_after_table_ends() == 0
-
-
-def check_only_adding_nodes(tables):
-    number_of_tables_in_previous_layer = random.randint(1, 15)
-    number_of_tables_in_next_layer = random.randint(1, 15)
-
-    def get_tables_starting_indices():
-        return list(map(lambda t: t.table_starting_index, tables))
-
-    starting_indices = get_tables_starting_indices()
-
-    tables[0].create_new_node_and_add_to_table(number_of_tables_in_previous_layer, number_of_tables_in_next_layer)
-    starting_indices[0] = 0
-    assert get_tables_starting_indices() == starting_indices
-
-    # adding more nodes to the same table does not change its starting index
-    tables[0].create_new_node_and_add_to_table(number_of_tables_in_previous_layer, number_of_tables_in_next_layer)
-    assert get_tables_starting_indices() == starting_indices
-
-
-def check_adding_and_removing_nodes(tables):
-    number_of_tables_in_previous_layer = random.randint(1, 15)
-    number_of_tables_in_next_layer = random.randint(1, 15)
-
-    def get_tables_starting_indices():
-        return list(map(lambda t: t.table_starting_index, tables))
-
-    starting_indices = get_tables_starting_indices()
-
-    tables[0].create_new_node_and_add_to_table(number_of_tables_in_previous_layer, number_of_tables_in_next_layer)
-    starting_indices[0] = 0
-    assert get_tables_starting_indices() == starting_indices
-
-    # adding more nodes to the same table does not change its starting index
-    tables[0].create_new_node_and_add_to_table(number_of_tables_in_previous_layer, number_of_tables_in_next_layer)
-    assert get_tables_starting_indices() == starting_indices
-
-    # deleting 1 of 2 nodes does not change the starting index of the table
-    tables[0].delete_node(0)
-    assert get_tables_starting_indices() == starting_indices
-
-    # deleting the second node should change the starting index
-    tables[0].delete_node(1)
-    starting_indices[0] = AbstractTable.INDEX_OF_START_IF_NO_NODES_IN_TABLE
-    assert get_tables_starting_indices() == starting_indices
-
-    # check that adding and removing nodes transfers well between tables
-    tables[0].create_new_node_and_add_to_table(number_of_tables_in_previous_layer, number_of_tables_in_next_layer)
-    tables[0].create_new_node_and_add_to_table(number_of_tables_in_previous_layer, number_of_tables_in_next_layer)
-    tables[4].create_new_node_and_add_to_table(number_of_tables_in_previous_layer, number_of_tables_in_next_layer)
-    starting_indices[0] = 0
-    starting_indices[4] = 2
-    assert get_tables_starting_indices() == starting_indices
-
-    tables[0].delete_node(0)
-    starting_indices[4] -= 1
-    assert get_tables_starting_indices() == starting_indices
-
-    # now start checking moving nodes between tables
-    node_added_to_table_0 = tables[0].create_new_node_and_add_to_table(number_of_tables_in_previous_layer,
-                                                                       number_of_tables_in_next_layer)
-    tables[1].create_new_node_and_add_to_table(number_of_tables_in_previous_layer, number_of_tables_in_next_layer)
-    starting_indices[1] = 2
-    starting_indices[4] = starting_indices[1] + 1
-    assert get_tables_starting_indices() == starting_indices
-
-    # now move a node from table 0 to table 2
-    tables[2].add_existing_node_to_table(tables[0], node_added_to_table_0)
-    starting_indices[1] -= 1
-    starting_indices[2] = starting_indices[1] + 1
-    starting_indices[4] = starting_indices[2] + 1
-
-    assert get_tables_starting_indices() == starting_indices
-
-
-
 if __name__ == "__main__":
     num_of_tables = random.randint(5, 7)
     # first check the tables that support deletion
     function_to_create_tables = get_TableSupportsDeletion
     tables = function_to_create_tables(num_of_tables)
     check_all_tables_have_zero_nodes(tables)
-    check_starting_index_is_not_initialized(tables)
-    check_adding_and_removing_nodes(tables)
 
     # now check the tables that don't support deletion
     function_to_create_tables = get_TableDoesntSupportsDeletion
     tables = function_to_create_tables(num_of_tables)
     check_all_tables_have_zero_nodes(tables)
-    check_starting_index_is_not_initialized(tables)
-    check_only_adding_nodes(tables)
 
     # testing the arnode tables would be done when we would test the layers since
     # the key functions there would be split and merge functions which would require different layers
